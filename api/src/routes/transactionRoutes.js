4b16710da7d9b2286a325727eb424097e06106cb
taskifbin <taskifbin@gmail.com>
2026-08-16 11:51:51 +0600

    Client server

--- api/src/routes/transactionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const transactionController = require("../controllers/transactionController");
-const { requireAuth } = require("../middleware/auth");
-
-router.get("/", requireAuth, transactionController.getHistory);
-
-module.exports = router;
--- api/src/server.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`payNEXT API listening on port ${port}`));
